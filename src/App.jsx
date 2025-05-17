17bbe79a1710bc0db172055bed7d5521707d2bfb
Nutty <tanut.ktr@gmail.com>
2025-05-17 18:42:50 +0700

    Add file Introduction and Navbar file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import "./App.css";
-import Gallery from "./component/Gallery";
+import Introduction from "./component/Introduction";
 
 function App() {
   return (
-    <div className="flex justify-center h-screen bg-gray-500 ">
-      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%] ">
-        <h1 className="w-full p-6 bg-white font-extrabold border-3 rounded-2xl">
-          React App Starter
-        </h1>
-        <section className="w-full p-5 bg-white flex border-3 rounded-2xl">
-          <ul className="list-inside list-disc flex-1">
-            <span className="font-semibold">Tech Stack:</span>
-            <li>Vite</li>
-            <li>React</li>
-            <li>JavaScript</li>
-            <li>Tailwind</li>
-          </ul>
-        </section>
-      <Gallery />
+    <div className="flex justify-center h-scree">
+      <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
+      <Introduction />
       </div>
     </div>
   );
---
+++ src/component/Introduction.jsx
@@ -0,0 +1,11 @@
+export default function Introduction(){
+
+
+return (
+<section>
+    <h1 className="font-bold text-[1.5rem]">Tanut Ketrueang</h1>
+    <h2>Full Stack Developer | Backend Developer | QA Tester</h2>
+</section>
+
+)
+}
